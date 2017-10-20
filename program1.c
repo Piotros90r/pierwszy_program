994dd4d4c592b9e5a7d1986e6b2c47d6d483a9e5
Piotros90r <lukasz.piotrowski@onet.pl>
2017-10-20 09:39:05 +0200

    interpretacja pierwszych programow w c

---
+++ program1.c
@@ -0,0 +1,22 @@
+#include <stdio.h> /* bibloteka standardowa imput-autput *//* y = f(x) */
+/*     f() => 0 */
+int main() /* funkcja glowna programu*/
+/* int -> typ zwracanej wartosci ( int- wartosc calkowita) */
+
+{
+
+
+puts("Pierwszy program"); /* puts - funkcja z biblioteki standardowej*/
+                          /* wysietlajacy ciag znakow*/
+                          /* wyswietlajaca ciag znakow*/
+                          /* automatycznie przechodzi do nowej lini*/
+putchar('g');
+putchar('\n'); /*znak konca lini*/
+putchar('\t'); /*tabulator*/
+putchar('\r'); /*powrot karetki- kursor powraca na poczatek lini*/
+putchar('\\'); /*wypisuje \ */
+ /* klamry oznaczaja blok programu, zwykle cialo funkcji*/
+return 0;
+
+
+}
