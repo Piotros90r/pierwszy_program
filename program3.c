12a9560017f3cf599154bf804881a21c3e83dbc7
Piotros90r <lukasz.piotrowski@onet.pl>
2017-10-20 14:54:54 +0200

    zadanie kalkulator zakonczono

--- program3.c
+++ program3.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
+/*kalkulator*/
+
 int mnoz(int a, int b){ return a*b; }
 int dodaj(int a, int b){ return a+b; }
-
+int odejmnij(int a, int b){ return a-b; }
+int dziel(int a, int b){ return a/b; }
 
 
 int main(void)
 {
 	int a=2, b=5; /* deklaracja wraz z inicjalizacja */
 	char wybor ='*';
-	/* if {wyrazenie} {blok jesli wyrazenie prawdziwe} */
-	/* else {blok jesli wyrazenie falszywe} */
 	
-	if(wybor == '*'){ /*sprawdzenie czy rowne, to nie jest '=' */
-		printf("Wynik mnozenia: %i/n", mnoz(a, b));
-	}else{
-		printf("Wynik dodawania: %i\n", dodaj(a, b));
-} /* ten srednik jest opcjonalny jesli wystepuje klamra */
-		return 0;
-	
-	int wynik;
-	wynik = mnoz(4,5);
-	printf("wynik mnozenia: %i\n", wynik);
+	scanf("%c", &wybor);
+
+	if(wybor == '*'){printf("wynik mnozenia %i\n", mnoz(a, b)); }
+
+	if(wybor == '/'){printf("wynik dzielenia %i\n", dziel(a, b)); } 
+
+	if(wybor == '+'){printf("wynik dodawania %i\n", dodaj(a, b)); } 
+
+	if(wybor == '-'){printf("wynik odejmowania %i\n", odejmnij(a, b)); } 
+
+/* gdy zaden warunek nie jest spelniony */
+
+
+	printf("Niewlasciwy znak");
+	printf("Error Haslo Bledne Haslo");
 		return 0;
 }
